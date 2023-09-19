c1480a15f4e37f6d7d4d9e77e04b103d5cb89dc3
Monisha T K <85619495+kothairagu@users.noreply.github.com>
2023-09-19 14:04:53 +0530

    Time: 1 ms (79.18%) | Memory: 40.4 MB (90.16%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder result = new StringBuilder();
+        int i = 0, j = 0;
+
+        while (i < word1.length() || j < word2.length()) {
+            if (i < word1.length()) {
+                result.append(word1.charAt(i));
+                i++;
+            }
+
+            if (j < word2.length()) {
+                result.append(word2.charAt(j));
+                j++;
+            }
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+        String word1 = scanner.nextLine();
+
+        
+        String word2 = scanner.nextLine();
+
+        Solution solution = new Solution();
+        String merged = solution.mergeAlternately(word1, word2);
+
+        System.out.println( merged);
+    }
+}
